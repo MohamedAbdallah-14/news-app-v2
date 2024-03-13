2288bcbd9acbeb594ae25c17e4a9c96b43756f00
Mohamed Abdallah <mohamedmabdallah14@gmail.com>
2024-03-14 01:59:31 +0200

    Add dependency injection for home module

--- lib/core/dependacy_injection/di.dart
+++ lib/core/dependacy_injection/di.dart
@@ -1,8 +1,14 @@
 import 'package:get_it/get_it.dart';
 import 'package:news_app/core/data/api_manager/api_manager.dart';
+import 'package:news_app/home/data/home_repository.dart';
+import 'package:news_app/home/di/home_di.dart';
 
 final di = GetIt.instance;
 
-void initDependencyInjection() {
+void initDependencyInjection({
+  HomeRepository? homeRepository,
+}) {
   di.registerLazySingleton(APIsManager.new);
+
+  HomeDI(di, repository: homeRepository);
 }
