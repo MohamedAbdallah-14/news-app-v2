a7d6d6e0059b13d3ca7e83dce83afb0b74efd6ec
Mohamed Abdallah <mohamedmabdallah14@gmail.com>
2024-03-14 00:11:59 +0200

    setup flavors

---
+++ lib/flavor.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+final testMode = Platform.environment.containsKey('FLUTTER_TEST');
+bool get liveEnvironment => AppFlavor.instance.flavor == Flavor.production;
+bool get devEnvironment => AppFlavor.instance.flavor == Flavor.dev;
+bool get mockEnvironment => AppFlavor.instance.flavor == Flavor.mock;
+
+enum Flavor { dev, production, mock }
+
+class AppFlavor {
+  AppFlavor._();
+  static final _instance = AppFlavor._();
+  static AppFlavor get instance => _instance;
+
+  Flavor flavor = Flavor.dev;
+
+  String get baseUrl {
+    switch (flavor) {
+      case Flavor.production:
+        return 'https://615d4dea12571a001720752d.mockapi.io/';
+      case Flavor.dev:
+        return 'https://615d4dea12571a001720752d.mockapi.io/';
+      case Flavor.mock:
+        return 'http://localhost/';
+    }
+  }
+}
