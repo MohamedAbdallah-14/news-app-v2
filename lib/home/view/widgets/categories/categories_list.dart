49d38aa58d1086dc0a08f170bd99633884806a34
Mohamed Abdallah <mohamedmabdallah14@gmail.com>
2024-03-14 05:38:22 +0200

    Add animation to expand and collapse container in CategoriesList

--- lib/home/view/widgets/categories/categories_list.dart
+++ lib/home/view/widgets/categories/categories_list.dart
@@ -7,24 +7,58 @@ import 'package:news_app/home/view/widgets/categories/scroll_button.dart';
 
 class CategoriesList extends StatefulWidget {
   const CategoriesList({
+    required this.isHidden,
     super.key = const Key('CategoriesList'),
   });
+
+  final bool isHidden;
 
   @override
   State<CategoriesList> createState() => _CategoriesListState();
 }
 
-class _CategoriesListState extends State<CategoriesList> {
+class _CategoriesListState extends State<CategoriesList>
+    with SingleTickerProviderStateMixin {
   final ScrollController _scrollController = ScrollController();
+  late AnimationController controller;
+  late Animation<double> animation;
 
   @override
   void initState() {
     super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    animation = CurvedAnimation(
+      parent: controller,
+      curve: Curves.easeOutCubic,
+    );
+
+    expandContainer();
   }
 
   @override
   void dispose() {
+    controller.dispose();
     super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant CategoriesList oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    expandContainer();
+  }
+
+  void expandContainer() {
+    if (widget.isHidden) {
+      controller.reverse();
+    } else {
+      controller.forward();
+    }
   }
 
   @override
@@ -36,38 +70,42 @@ class _CategoriesListState extends State<CategoriesList> {
           constraints: const BoxConstraints(
             maxWidth: kIsWeb ? 840 : double.infinity,
           ),
-          child: SizedBox(
-            key: const Key('CategoriesListSizedBox'),
-            height: 150,
-            child: Row(
-              key: const Key('CategoriesListRow'),
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ScrollButton(
-                  key: const Key('CategoriesListScrollButtonLeft'),
-                  controller: _scrollController,
-                  isLeft: true,
-                ),
-                Expanded(
-                  key: const Key('CategoriesListExpanded'),
-                  child: ListView.builder(
-                    key: const Key('CategoriesListListView'),
+          child: SizeTransition(
+            key: const Key('CategoriesListSizeTransition'),
+            sizeFactor: animation,
+            child: SizedBox(
+              key: const Key('CategoriesListSizedBox'),
+              height: 150,
+              child: Row(
+                key: const Key('CategoriesListRow'),
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ScrollButton(
+                    key: const Key('CategoriesListScrollButtonLeft'),
                     controller: _scrollController,
-                    scrollDirection: Axis.horizontal,
-                    itemCount: state.categories?.length ?? 0,
-                    itemBuilder: (context, index) => CategoryCell(
-                      category: state.categories![index],
-                      isSelected:
-                          state.selectedCategory == state.categories?[index],
+                    isLeft: true,
+                  ),
+                  Expanded(
+                    key: const Key('CategoriesListExpanded'),
+                    child: ListView.builder(
+                      key: const Key('CategoriesListListView'),
+                      controller: _scrollController,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: state.categories?.length ?? 0,
+                      itemBuilder: (context, index) => CategoryCell(
+                        category: state.categories![index],
+                        isSelected:
+                            state.selectedCategory == state.categories?[index],
+                      ),
                     ),
                   ),
-                ),
-                ScrollButton(
-                  key: const Key('CategoriesListScrollButtonRight'),
-                  controller: _scrollController,
-                  isLeft: false,
-                ),
-              ],
+                  ScrollButton(
+                    key: const Key('CategoriesListScrollButtonRight'),
+                    controller: _scrollController,
+                    isLeft: false,
+                  ),
+                ],
+              ),
             ),
           ),
         );
