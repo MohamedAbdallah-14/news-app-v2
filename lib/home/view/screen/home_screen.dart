baf34716664298eff69ebee944a71f91d2957f12
Mohamed Abdallah <mohamedmabdallah14@gmail.com>
2024-03-14 05:38:09 +0200

    Add favorites functionality to HomeScreen

--- lib/home/view/screen/home_screen.dart
+++ lib/home/view/screen/home_screen.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/core/helpers/app_colors.dart';
 import 'package:news_app/core/view/widgets/app_screen.dart';
+import 'package:news_app/home/logic/favorites/favorites_cubit.dart';
 import 'package:news_app/home/logic/news/news_cubit.dart';
 import 'package:news_app/home/view/widgets/categories/categories_list.dart';
+import 'package:news_app/home/view/widgets/home_app_bar.dart';
+import 'package:news_app/home/view/widgets/news/favorites_list.dart';
 import 'package:news_app/home/view/widgets/news/news_list.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -20,15 +23,29 @@ class HomeScreen extends StatelessWidget {
             onRefresh: () async {
               await context.read<NewsCubit>().reload();
             },
-            child: const SafeArea(
+            child: SafeArea(
               child: SizedBox(
                 width: double.infinity,
-                child: Column(
-                  children: [
-                    SizedBox(height: 16),
-                    CategoriesList(),
-                    NewsList(),
-                  ],
+                child: BlocBuilder<FavoritesCubit, FavoritesState>(
+                  builder: (context, state) {
+                    return Column(
+                      children: [
+                        const SizedBox(height: 16),
+                        const HomeAppBar(),
+                        CategoriesList(
+                          isHidden: state.showFavorites,
+                        ),
+                        Visibility(
+                          visible: !state.showFavorites,
+                          child: const Expanded(child: NewsList()),
+                        ),
+                        Visibility(
+                          visible: state.showFavorites,
+                          child: const Expanded(child: FavoritesList()),
+                        ),
+                      ],
+                    );
+                  },
                 ),
               ),
             ),
